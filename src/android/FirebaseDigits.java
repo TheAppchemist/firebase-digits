d96cf47f72b9b6bb789040fc0ac17e8b27050565
Melvin Musehani <melvin@appchemy.co.za>
2017-10-04 21:53:48 +0200

    Work in progress

--- src/android/FirebaseDigits.java
+++ src/android/FirebaseDigits.java
@@ -2,16 +2,73 @@ package za.co.appchemy.plugins.digits;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+
+import android.os.Bundle;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * This class echoes a string called from JavaScript.
  */
 public class FirebaseDigits extends CordovaPlugin {
+    private CallbackContext callbackContext = null;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential credential) {
+            // This callback will be invoked in two situations:
+            // 1 - Instant verification. In some cases the phone number can be instantly
+            //     verified without needing to send or enter a verification code.
+            // 2 - Auto-retrieval. On some devices Google Play services can automatically
+            //     detect the incoming verification SMS and perform verificaiton without
+            //     user action.
+
+            FirebaseDigits.this.callbackContext.success("verification completed");
+            // signInWithPhoneAuthCredential(credential);
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // This callback is invoked in an invalid request for verification is made,
+            // for instance if the the phone number format is not valid.
+            FirebaseDigits.this.callbackContext.success("onVerificationFailed");
+
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+                // ...
+            } else if (e instanceof FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+                // ...
+            }
+
+            // Show a message and update the UI
+            // ...
+        }
+
+        @Override
+        public void onCodeSent(String verificationId,
+                               PhoneAuthProvider.ForceResendingToken token) {
+            // The SMS verification code has been sent to the provided phone number, we
+            // now need to ask the user to enter the code and then construct a credential
+            // by combining the code with a verification ID.
+
+            // Save verification ID and resending token so we can use them later
+//        mVerificationId = verificationId;
+//        mResendToken = token;
+
+            FirebaseDigits.this.callbackContext.success("Code sent");
+            // ...
+        }
+    };
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -19,10 +76,10 @@ public class FirebaseDigits extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
-        } else if (action.equals('verifyPhoneNumber')) {
+        } else if (action.equals("verifyPhoneNumber")) {
             String phoneNumber = args.getString(0);
             this.verifyPhoneNumber(phoneNumber, callbackContext);
-            return true
+            return true;
         }
         return false;
     }
@@ -36,15 +93,17 @@ public class FirebaseDigits extends CordovaPlugin {
     }
 
     private void verifyPhoneNumber(String phoneNumber, CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+
         if (phoneNumber == null || phoneNumber.length() == 0) {
-            callbackContext.error('Phone number needed')
+            callbackContext.error("Phone number needed");
         } else {
             PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                phoneNumber,        // Phone number to verify
-                60,                 // Timeout duration
-                TimeUnit.SECONDS,   // Unit of timeout
-                this,               // Activity (for callback binding)
-                mCallbacks);        // OnVerificationStateChangedCallbacks
+                    phoneNumber,        // Phone number to verify
+                    60,                 // Timeout duration
+                    TimeUnit.SECONDS,   // Unit of timeout
+                    webView.getActivity(),               // Activity (for callback binding)
+                    mCallbacks);        // OnVerificationStateChangedCallbacks
         }
     }
 }
